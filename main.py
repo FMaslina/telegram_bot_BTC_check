e4a53efdaaa851b3efa770617eb1ea9634a94eee
FMaslina <kostyan_20044@mail.ru>
2022-07-02 19:15:27 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime
+import telebot
+
+token = "2102479738:AAEI9IQZKWTl18tAS1Aa8hqKA3U-X749WqA"
+bot = telebot.TeleBot(token)
+
+
+req = requests.get('https://yobit.net/api/3/ticker/btc_usd')
+responce = req.json()
+sell_price = round(responce['btc_usd']['sell'], 2)
+buy_price = round(responce['btc_usd']['buy'], 2)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Введи "цена", чтобы узнать цену BTC')
+
+@bot.message_handler(content_types=['text'])
+def tell_price(message):
+    try:
+        if message.text.lower() == 'цена':
+            bot.send_message(message.chat.id, f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
+                             f'Продажа: {sell_price}\nПокупка: {buy_price}')
+        else:
+            bot.send_message(message.chat.id, 'Я слишком глупый, и знаю только команду /start и слово "Цена"!')
+    except:
+        bot.send_message(message.chat.id, 'Что-то пошло не так!')
+
+bot.polling()
